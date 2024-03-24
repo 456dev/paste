787177bdc4562b89278432b09e158888e719e38a
Isaac - The456 <the456@the456gamer.dev>
2024-03-24 22:14:27 +0000

    add wordwrap preference, pass into controls and text area

--- src/components/EditorControls.tsx
+++ src/components/EditorControls.tsx
@@ -19,6 +19,8 @@ export interface EditorControlsProps {
   setReadOnly: (value: boolean) => void;
   theme: keyof Themes;
   setTheme: (value: keyof Themes) => void;
+  wordWrap: boolean
+  setWordWrap: (value: boolean) => void;
   zoom: (delta: number) => void;
 }
 
@@ -31,6 +33,8 @@ export default function EditorControls({
   setReadOnly,
   theme,
   setTheme,
+  wordWrap,
+  setWordWrap,
   zoom,
 }: EditorControlsProps) {
   const [saving, setSaving] = useState<boolean>(false);
--- src/components/EditorTextArea.tsx
+++ src/components/EditorTextArea.tsx
@@ -27,6 +27,7 @@ export interface EditorTextAreaProps {
   language: string;
   fontSize: number;
   readOnly: boolean;
+  wordWrap: boolean
   resetFunction: MutableRefObject<ResetFunction | undefined>;
 }
 
@@ -38,6 +39,7 @@ export default function EditorTextArea({
   language,
   fontSize,
   readOnly,
+  wordWrap,
   resetFunction,
 }: EditorTextAreaProps) {
   const [editor, setEditor] = useState<editor.IStandaloneCodeEditor>();
